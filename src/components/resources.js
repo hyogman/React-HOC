5afb9b6cf8f1b01a88b8a3685909e6d9edd856e6
henry <henryyogman@gmail.com>
2016-08-02 12:02:03 -0600

    created baseline redux for sign in

---
+++ src/components/resources.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default (props) => {
+
+  return (
+    <div>
+    Super Secret Recipe (Mock Data)
+      <ul>
+        <li>1 cup sugar</li>
+        <li>1 cup pepper</li>
+        <li>2 cups salt</li>
+      </ul>
+    </div>
+
+  );
+
+}
